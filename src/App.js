4126d4474525b0c7bb23d990abfe133f2d53c263
Muhammad Umar Saleem <umars62350@gmail.com>
2024-05-08 16:55:17 +0200

    Create a list using a Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import { useState } from "react";
 
-function App()
-  {
-    const [name,setName]=useState("");
-    const [tnc,setTnc]=useState(false);
-    const [interest, setInterest]=useState("");
-    function getFormData (e)
-    {
-      console.warn(name,tnc,interest)
-      e.preventDefault()
-    }
-    return (
-<div className="App">
-  <h1>Handle form in the React</h1>
-  <form onSubmit={getFormData}>
-    <input type="text" placeholder="enter name" value={name} onChange={(e)=>setName(e.target.value)}/> <br /><br />
-<select onChange={(e)=>setInterest(e.target.value)}>
-<option>Select option</option>
-<option>Chishtian</option>
-<option>Horsens</option>
-</select><br/><br/>
-<input type="checkbox" onChange={(e)=>setTnc(e.target.checked)}/> <span> Accept Term and conditions</span>
-<br/><br/>
-<button type="submit">Submit</button>
-<button>Clear</button>
-  </form>
-</div>
-    )
-  }
-  export default App;
+import './App.css';
+import React from "react";
+import{Table} from 'react-bootstrap'
+
+function App(){
+const user = [
+  {name:'umar', email:'umar@gmail.com', mobile:'343434'},
+  {name:'usman', email:'usman@gmail.com', mobile:'454545'},
+  {name:'Ali', email:'ali@gmail.com', mobile:'232323'},
+  {name:'Ibrahim', email:'ibrahim@gmail.com', mobile:'565656'},
+]
+  return (
+    <div className="App">
+      <h1>List with Booststrap Table</h1>
+      <Table striped variant="dark">
+        <tbody>
+          <tr>
+          <td>Name</td>  
+          <td>Email</td>
+          <td>Mobile</td>
+          </tr>
+          {
+            user.map((item,i)=>
+            // item.mobile==='111222'?
+            <tr key={i}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.mobile}</td>
+              </tr>
+          /* </tr>:null */
+            )
+          }
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+export default App;
